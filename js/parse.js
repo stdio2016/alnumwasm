0a715b868007c2c85e73c48b90768bec559004ee
stdio2016 <randomfrank8@gmail.com>
2018-09-14 21:31:22 +0800

    can parse int32, float, br and memory ops

--- js/parse.js
+++ js/parse.js
@@ -14,7 +14,20 @@ function AlnumWasmParser(str) {
   this.code = [];
 }
 
+AlnumWasmParser.log2 = function (n, strict) {
+  if ((n & (n-1) || n !== n || n < 0) && strict) {
+    throw SyntaxError(n + ' is not power of 2');
+  }
+  var lv = 0;
+  while (n > 1) {
+    lv++;
+    n >>= 1;
+  }
+  return lv;
+};
+
 AlnumWasmParser.writeInt = function (n, code) {
+  n = n | 0;
   while (n >= 64 || n < -64) {
     code.push(n & 0x7F | 0x80);
     n >>= 7;
@@ -25,7 +38,7 @@ AlnumWasmParser.writeInt = function (n, code) {
 AlnumWasmParser.writeUint = function (n, code) {
   while (n >= 128) {
     code.push(n & 0x7F | 0x80);
-    n >>= 7;
+    n >>>= 7;
   }
   code.push(n);
 };
@@ -52,7 +65,19 @@ AlnumWasmParser.prototype.parseExpr = function () {
       this.labelNames[lbl[0]] = lbl[1];
     }
     else if (op === OpCodes.BR || op === OpCodes.BRIF) {
-      
+      var name = this.lexer.next();
+      var lv;
+      if (/^\d+$/.test(name)) {
+        lv = parseInt(name);
+      }
+      else {3
+        lv = this.labelNames[name];
+        if (lv === void 0) {
+          throw ReferenceError("label " + name + " is not defined");
+        }
+        else lv = this.scope.length - lv;
+      }
+      AlnumWasmParser.writeUint(lv, this.code);
     }
     else if (op === OpCodes.BRTABLE) {
       
@@ -78,7 +103,7 @@ AlnumWasmParser.prototype.parseExpr = function () {
         AlnumWasmParser.writeUint(this.locals[vname], this.code);
       }
       else {
-        throw SyntaxError("variable " + vname + " is not defined");
+        throw ReferenceError("variable " + vname + " is not defined");
       }
     }
     else if (op >= OpCodes.GETGLOBAL && op <= OpCodes.SETGLOBAL) {
@@ -91,13 +116,16 @@ AlnumWasmParser.prototype.parseExpr = function () {
       }
     }
     else if (op >= OpCodes.I32LOAD && op <= OpCodes.I64STORE32) {
-      
+      this.parseMemoryOp();
     }
     else if (op === OpCodes.CURRENTMEMORY || op === OpCodes.GROWMEMORY) {
       this.code.push(0);
     }
-    else if (op === OpCodes.I32CONST || op === OpCodes.I64CONST) {
-      this.parseInt(op - OpCodes.I32CONST);
+    else if (op === OpCodes.I32CONST) {
+      this.parseInt();
+    }
+    else if (op === OpCodes.I64CONST) {
+      ;
     }
     else if (op === OpCodes.F32CONST || op === OpCodes.F64CONST) {
       this.parseFloat(op - OpCodes.F32CONST);
@@ -131,6 +159,65 @@ AlnumWasmParser.prototype.parseBlockOp = function () {
   else {
     this.lexer.backtrack();
     this.code.push(0x40);
+  }
+};
+
+AlnumWasmParser.prototype.parseMemoryOp = function () {
+  var tok = this.lexer.next();
+  var align = 0;
+  if (tok === "ALIGN") {
+    tok = this.lexer.next();
+    if (!tok) throw SyntaxError("missing alignment");
+    if (!/^\d+$/.test(tok)) throw SyntaxError("alignment is not an integer");
+    align = AlnumWasmParser.log2(parseInt(tok), true);
+  }
+  else this.lexer.backtrack();
+  tok = this.lexer.next();
+  var offset = 0;
+  if (tok === "OFFSET" || tok === "OFF") {
+    tok = this.lexer.next();
+    if (!tok) throw SyntaxError("missing offset");
+    if (!/^\d+$/.test(tok)) throw SyntaxError("offset is not an integer");
+    offset = parseInt(tok);
+  }
+  else this.lexer.backtrack();
+  AlnumWasmParser.writeUint(align, this.code);
+  AlnumWasmParser.writeUint(offset, this.code);
+};
+
+AlnumWasmParser.prototype.parseInt = function () {
+  var tok = this.lexer.next();
+  if (!tok) throw SyntaxError("missing integer");
+  var sign = 1;
+  var n = tok;
+  if (n[0] === 'M') { // minus
+    n = n.substring(1);
+    sign = -1;
+  }
+  n = parseInt(n);
+  if (n !== n) throw SyntaxError(tok + ' is not an integer');
+  AlnumWasmParser.writeInt(n * sign, this.code);
+};
+
+AlnumWasmParser.prototype.parseFloat = function (isDouble) {
+  var tok = this.lexer.next();
+  if (!tok) throw SyntaxError("missing float");
+  tok = tok.replace("P", ".");
+  tok = tok.replace(/M/g, "-");
+  var n = parseFloat(tok);
+  var dv = new DataView(new ArrayBuffer(8));
+  var d;
+  if (isDouble) {
+    dv.setFloat64(0, n, true);
+    d = dv.getUint32(0, true);
+    this.code.push(d & 0xff, d>>8 & 0xff, d>>16 & 0xff, d>>24 & 0xff);
+    d = dv.getUint32(4, true);
+    this.code.push(d & 0xff, d>>8 & 0xff, d>>16 & 0xff, d>>24 & 0xff);
+  }
+  else {
+    dv.setFloat32(0, n, true);
+    d = dv.getUint32(0, true);
+    this.code.push(d & 0xff, d>>8 & 0xff, d>>16 & 0xff, d>>24 & 0xff);
   }
 };
 
