73a34d0e8db1711e09fdf868bcb01460677cdd01
stdio2016 <randomfrank8@gmail.com>
2018-10-03 01:31:39 +0800

    fix knight tour example

--- test/knight.js
+++ test/knight.js
@@ -18,11 +18,16 @@ function gotScript(e) {
     return WebAssembly.instantiate(asm, {env: env});
   }).then(inst => {
     knightModule = inst;
-    console.time('Knight tour');
+    var t = +new Date();
     knightModule.exports.knight(0, 0, 1);
-    console.timeEnd('Knight tour');
+    var cost = new Date() - t;
     var sol = new Int32Array(knightModule.exports.solution.buffer, 0, 64);
-    console.log(sol);
+    
+    var txt = document.createTextNode('alnumwasm took ' + cost + ' ms');
+    var p = document.createElement('p');
+    p.appendChild(txt);
+    document.body.append(p);
+
     var tb = document.createElement('table');
     for (var i = 0; i < 8; i++) {
       var r = tb.insertRow();
@@ -40,3 +45,58 @@ function gotScript(e) {
 
 loadScript();
 };
+
+var cnt = 0;
+var steps = new Int32Array(64);
+function knight(x, y, lv) {
+  cnt += 1;
+  // bound check
+  if (x >= 8 || x < 0 || y >= 8 || y < 0) return 0;
+  
+  // get array index
+  var pos = x + y * 8;
+  
+  // test if a square is visited
+  if (steps[pos]) return 0;
+  steps[pos] = lv;
+  
+  // test if finished
+  if (lv >= 64) return 1;
+  
+  lv += 1;
+  if (knight(x+1, y+2, lv)) return 1;
+  if (knight(x+2, y+1, lv)) return 1;
+  if (knight(x+2, y-1, lv)) return 1;
+  if (knight(x+1, y-2, lv)) return 1;
+  if (knight(x-1, y-2, lv)) return 1;
+  if (knight(x-2, y-1, lv)) return 1;
+  if (knight(x-2, y+1, lv)) return 1;
+  if (knight(x-1, y+2, lv)) return 1;
+  
+  // no moves
+  steps[pos] = 0;
+  return 0;
+}
+
+function testKnightJs() {
+  cnt = 0;
+  steps.fill(0);
+  var t = +new Date();
+  knight(0, 0, 1);
+  var cost = new Date() - t;
+  
+  var txt = document.createTextNode('javascript took ' + cost + ' ms');
+  var p = document.createElement('p');
+  p.appendChild(txt);
+  document.body.append(p);
+
+  var tb = document.createElement('table');
+  for (var i = 0; i < 8; i++) {
+    var r = tb.insertRow();
+    for (var j = 0; j < 8; j++) {
+      var c = r.insertCell();
+      c.innerHTML = steps[i*8 + j];
+    }
+  }
+  document.body.append(tb);
+}
